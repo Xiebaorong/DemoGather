9c476af820e4c8b975e68e5c1a0556020a092693
谢宝荣 <xiebr@7invensun.com>
2018-08-20 10:47:17 +0800

    视频未播放时，点击窗口转换程序闪退。 修改：添加MediaController.setMediaPlayer(this);

--- app/src/main/java/com/example/a7invensun/verifydemo/videopaly/VideoPalyActivity.java
+++ app/src/main/java/com/example/a7invensun/verifydemo/videopaly/VideoPalyActivity.java
@@ -2,25 +2,24 @@ package com.example.a7invensun.verifydemo.videopaly;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import com.example.a7invensun.verifydemo.R;
 import com.example.a7invensun.verifydemo.logdemo.util.LogUtils;
 import com.universalvideoview.UniversalMediaController;
 import com.universalvideoview.UniversalVideoView;
 
-public class VideoPalyActivity extends AppCompatActivity implements UniversalVideoView.VideoViewCallback {
+public class VideoPalyActivity extends AppCompatActivity implements UniversalVideoView.VideoViewCallback, UniversalMediaController.MediaPlayerControl {
     private UniversalVideoView mVideoView;
     private UniversalMediaController mMediaController;
     private FrameLayout mVideoLayout;
-//    private LinearLayout mBottomLayout;
+    //    private LinearLayout mBottomLayout;
     private boolean isFullscreen;
     private int height;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +34,16 @@ public class VideoPalyActivity extends AppCompatActivity implements UniversalVid
         mVideoLayout = findViewById(R.id.video_layout);
         mVideoView.setMediaController(mMediaController);
 
-
+        mMediaController.setTitle("动画片");
+//        mMediaController.hideLoading();
+        mMediaController.show();
+       ;
+        if (!mVideoView.isPlaying()) {
+            LogUtils.e("判断是否为空"+ mVideoView.isPlaying());
+        }
+            mMediaController.hideComplete();
         mVideoView.setVideoViewCallback(this);
+        mMediaController.setMediaPlayer(this);
         Uri uri = Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
         mVideoView.setVideoURI(uri);
         mVideoView.start();
@@ -47,11 +54,12 @@ public class VideoPalyActivity extends AppCompatActivity implements UniversalVid
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         height = mVideoView.getHeight();
-        LogUtils.e(mVideoView.getHeight()+"");
+        LogUtils.e(mVideoView.getHeight() + "");
     }
 
     @Override
     public void onScaleChange(boolean isFullscreen) {
+        LogUtils.e("窗口改变");
         this.isFullscreen = isFullscreen;
         if (isFullscreen) {
             ViewGroup.LayoutParams layoutParams = mVideoLayout.getLayoutParams();
@@ -59,7 +67,7 @@ public class VideoPalyActivity extends AppCompatActivity implements UniversalVid
             layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
             mVideoView.setLayoutParams(layoutParams);
 //            mBottomLayout.setVisibility(View.GONE);
-        }else {
+        } else {
             ViewGroup.LayoutParams layoutParams = mVideoLayout.getLayoutParams();
             layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
             layoutParams.height = this.height;
@@ -70,12 +78,12 @@ public class VideoPalyActivity extends AppCompatActivity implements UniversalVid
 
     @Override
     public void onPause(MediaPlayer mediaPlayer) {
-
+        LogUtils.e("onPause");
     }
 
     @Override
     public void onStart(MediaPlayer mediaPlayer) {
-
+        LogUtils.e("onStart");
     }
 
     @Override
@@ -85,6 +93,72 @@ public class VideoPalyActivity extends AppCompatActivity implements UniversalVid
 
     @Override
     public void onBufferingEnd(MediaPlayer mediaPlayer) {
+
+    }
+
+
+    @Override
+    public void start() {
+        LogUtils.e("播放");
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public void closePlayer() {
+
+    }
+
+    @Override
+    public void setFullscreen(boolean fullscreen) {
+
+    }
+
+    @Override
+    public void setFullscreen(boolean fullscreen, int screenOrientation) {
 
     }
 }
