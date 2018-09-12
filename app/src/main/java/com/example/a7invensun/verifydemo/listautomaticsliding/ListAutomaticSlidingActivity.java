5c5a5d2ed869fa3b665731c38f85dcd558942abf
谢宝荣 <xiebr@7invensun.com>
2018-09-12 10:13:33 +0800

    LIstView点击按钮自动滑动

---
+++ app/src/main/java/com/example/a7invensun/verifydemo/listautomaticsliding/ListAutomaticSlidingActivity.java
@@ -0,0 +1,13 @@
+package com.example.a7invensun.verifydemo.listautomaticsliding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListAutomaticSlidingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_automatic_sliding);
+    }
+}
